320508a96fa8f55ae22c5e4bc7b12619e288d40e
Rio <riostockton@gmail.com>
2021-11-09 15:02:05 +0000

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BugTracker.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(RoleViewModel role)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new()
+                {
+                    Name = role.Name
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return Redirect("/admin");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(role);
+        }
+    }
+}
---
+++ Models/RoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class RoleViewModel
+    {
+        [Required]
+        [DisplayName("Name of role")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
